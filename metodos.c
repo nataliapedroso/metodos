c49c16a16edaec8cf7f0ca6e0b22ff5dc3b7c58b
nataliapedroso <nataliapedroso@id.uff.br>
2017-09-28 15:09:59 -0300

    criação de arquivos e funções imprime e troca

---
+++ metodos.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<math.h>
+
+FILE *op;
+
+void imprime(double **M, int dim)
+{
+	int i, j;
+
+	for(i=0; i<dim; i++)
+	{
+		for(j=0; j<dim+1; j++)
+			printf("%lf\t", M[i][j]);
+		printf("\n");
+	}
+	printf("\n");
+}
+int troca(double **M, int dim, int n)
+{
+	int i, j, cont=0, k;
+	double aux;
+
+	for(i=n; i<dim; i++)
+	{
+		if(M[i][n]!=0)
+		{
+			for(j=0; j<dim+1; j++)
+			{
+				aux=M[n][j];
+				M[n][j]=M[i][j];
+				M[i][j]=aux;
+			}
+			i=dim;
+		}
+	}
+	cont++;
+	return(cont);
+}
+
